a743d8195e71bebcc271e015d4d209aa9b84b02f
alexander.lisachenko <alexander.lisachenko@alpari.org>
2013-02-06 16:45:38 +0200

    Increase nesting level for xdebug

--- www/index.php
+++ www/index.php
@@ -13,6 +13,9 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 include __DIR__ . '/../vendor/lisachenko/go-aop-php/src/Go/Core/AspectKernel.php';
 include __DIR__ . '/protected/extensions/go-aop-php/ApplicationAspectKernel.php';
 
+// Prevent an error about nesting level
+ini_set('xdebug.max_nesting_level', 500);
+
 // Initialize an application aspect container
 $applicationAspectKernel = ApplicationAspectKernel::getInstance();
 $applicationAspectKernel->init(array(
